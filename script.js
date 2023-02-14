b2546371a10fb394026647e27f30e213940f5b28
Kittencode91 <62710872+Kittencode91@users.noreply.github.com>
2023-02-14 14:34:54 +0000

    added frost effect to side nav, hamburger will now toggle, overhauled the JS

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ let meteorNumber = 25;
 for (let i = 1; i <= meteorNumber; i++) {
   body.innerHTML += `<div class="meteor-${i}"></div>`;
 }
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.email.focus();
+window.location.href = 'index.html';
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+window.location.href = 'index.html';
+return false;
+}
+}
+
+  function menuAnimation(x) {
+    x.classList.toggle("change");
+    var element = document.getElementById("slideNav");
+    element.classList.toggle("navSlide");
+    var textFade = document.getElementById("sideBarText");
+    textFade.classList.toggle("textFadeIn");
+  }
+
+smoothScroll.init();
